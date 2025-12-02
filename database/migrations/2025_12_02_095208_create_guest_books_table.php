bc9df2e56991adf3d60e435cdb153504990e6a4f
AkbarKheun <akbarmajenesmk5@gmail.com>
2025-12-02 17:04:39 +0700

    add table employee and guest books

---
+++ database/migrations/2025_12_02_095208_create_guest_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('guest_books', function (Blueprint $table) {
+            $table->id();
+            $table->string('guest_name');
+            $table->string('number_phone');
+            $table->string('agency');
+            $table->string('objective');
+            $table->unsignedBigInteger('employee_id');
+            $table->foreign('employee_id')->references('id')->on('employees')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('guest_books');
+    }
+};
